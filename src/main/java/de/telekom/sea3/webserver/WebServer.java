be974e0c2157575cfa4371edabad858649d3464d
Aleksandra Khvan <aleksandra.klochko@t-systems.com>
2021-06-11 15:58:02 +0300

    post request was added

--- src/main/java/de/telekom/sea3/webserver/WebServer.java
+++ src/main/java/de/telekom/sea3/webserver/WebServer.java
@@ -3,6 +3,7 @@ package de.telekom.sea3.webserver;
 import org.springframework.boot.SpringApplication;
 
 public class WebServer {
+
     public static void main(String[] args) {
         SpringApplication.run( WebServer.class, args);
     }
